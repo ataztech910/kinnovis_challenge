4faa702754c3eabd85b8abe145f2b2775fc44dd1
ataztech910 <webconsult.ekb@gmail.com>
2021-10-07 20:48:22 +0200

    fix AWS service

--- src/views/FirstTask.vue
+++ src/views/FirstTask.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="FirstTask">
-    <div class="FirstTask__items" v-for="item in inventory" :key="item.id">
-      <div>{{ item.sku }}</div>
-      <div>{{ item.name }}</div>
-      <div>{{ mutateDate(item.bestBeforeDate) }}</div>
-      <div>{{ item.inventoryLevel }}</div>
+    <div class="FirstTask__raw FirstTask__raw--header">
+      <div class="FirstTask__raw__cell">Code</div>
+      <div class="FirstTask__raw__cell">Name</div>
+      <div class="FirstTask__raw__cell">Date</div>
+      <div class="FirstTask__raw__cell">Level</div>
+      <div class="FirstTask__raw__cell"></div>
+    </div>
+    <div class="FirstTask__raw" v-for="item in inventory" :key="item.id">
+      <div class="FirstTask__raw__cell">{{ item.sku }}</div>
+      <div class="FirstTask__raw__cell">{{ item.name }}</div>
+      <div class="FirstTask__raw__cell">
+        {{ mutateDate(item.bestBeforeDate) }}
+      </div>
+      <div class="FirstTask__raw__cell">{{ item.inventoryLevel }}</div>
+      <div class="FirstTask__raw__cell">
+        <span>Edit</span>
+        <span>Delete</span>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +37,6 @@ export default class FirstTask extends Vue {
   @InventoryMutationNames setInventory: (newState: InventoryModel) => void;
   created(): void {
     this.initInventory();
-    console.log(this.inventory);
   }
 
   // This function here in case if API have a different formatting for dates like AWS for example
@@ -36,3 +48,18 @@ export default class FirstTask extends Vue {
   }
 }
 </script>
+<style lang="scss">
+.FirstTask {
+  display: table;
+  width: 100%;
+  &__raw {
+    display: table-row;
+    &__cell {
+      display: table-cell;
+      border-bottom: 1px solid #42b983;
+      text-align: left;
+      padding: 0.5rem;
+    }
+  }
+}
+</style>
